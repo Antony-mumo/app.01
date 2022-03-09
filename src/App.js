29a2f908452dac81ea7560e52b2644bfdf7aeb85
Antony-mumo <mumoantony00@gmail.com>
2022-03-09 11:24:32 +0300

    antony-mumo-dev

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Tonny.js welcome to levtech renovation project where by we shall be rethinking the whole idea and design structure plus functionality</h1>
+    
+
+    </div>
+  );
+}
+
+export default App;
